f52a47ec6a88f954f70152d43565b920dfe70ec0
nash115 <72687027+Nash115@users.noreply.github.com>
2024-02-22 09:53:41 +0000

    Ajout d'un "tree" et d'un code exemple

--- Assets/mission.py
+++ Assets/mission.py
@@ -5,10 +5,19 @@ def generate_pwd():
     leng = random.randint(6, 15)
     for i in range(leng):
         s += chr(random.randint(33, 126))
+    return "15082023" # The correct code in the tree
     return s
 
 def generate_tree():
-    tree = {}
+    tree = {
+        "private":{
+            "holidays":"I went to london : 2023-08-15,\nto paris : 2022-07-01,\nto new-york : 2021-06-15;\nI love travelling !",
+            "passwords":{
+                "administrator":"The new password is the date of my last holiday ! (ddmmyyyy)",
+                "my-phone":"The password of my phone is : 123456"
+            }
+        }
+    }
     return tree
 
 class Mission:
